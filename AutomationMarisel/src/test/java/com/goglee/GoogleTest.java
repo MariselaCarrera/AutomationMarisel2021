3e4d651eef4f35ad494df6a82f802edd323c5552
s3473686 <s3473686@LEXTSISMCG.mexeo.BNS>
2021-10-07 20:41:04 -0500

    commit

---
+++ AutomationMarisel/src/test/java/com/goglee/GoogleTest.java
@@ -0,0 +1,19 @@
+package com.goglee;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class GoogleTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
